787a3399a30f9aa45ed446c059dc1079f9fbe97d
purid <purid@DIWAKAR>
2020-03-31 10:19:14 +0530

    .

---
+++ src/main/java/com/niit/techno/crm/dao/TravelDAO.java
@@ -0,0 +1,7 @@
+package com.niit.techno.crm.dao;
+
+import com.niit.techno.crm.model.Travel;
+
+public interface TravelDAO {
+public void saveTravel(Travel tUser);
+}
---
+++ src/main/java/com/niit/techno/crm/dao/TravelDAOImpl.java
@@ -0,0 +1,23 @@
+package com.niit.techno.crm.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.techno.crm.model.Travel;
+
+@Repository
+public class TravelDAOImpl implements TravelDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	//@Override
+	public void saveTravel(Travel tUser) {
+		// TODO Auto-generated method stub
+Session currentSession = sessionFactory.getCurrentSession();
+currentSession.saveOrUpdate(tUser);
+	}
+
+}
