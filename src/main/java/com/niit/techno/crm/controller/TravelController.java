d9279fa440184a822e431e4c2bcb0b60963e9ef6
purid <purid@DIWAKAR>
2020-03-31 10:11:20 +0530

    .

---
+++ src/main/java/com/niit/techno/crm/controller/TravelController.java
@@ -0,0 +1,25 @@
+package com.niit.techno.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.niit.techno.crm.model.Travel;
+
+@Controller
+@RequestMapping("/travel")
+public class TravelController {
+
+	@GetMapping("/travelForm")
+	public String showFormForAdd(@RequestParam("customerId") int theId,Model theModel)
+{
+	Travel travelUser = new Travel();
+	theModel.addAttribute("cid",theId);
+	theModel.addAttribute("tuser",travelUser);
+	return "travel-form";
+	
+}
+	
+}
